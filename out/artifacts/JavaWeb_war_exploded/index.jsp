5efc300ae75e6957ebda3b3d1accb4bda3bdeebc
xiewenjing <xiewenjing@lakala.com>
2023-05-02 16:16:40 +0800

    first

---
+++ out/artifacts/JavaWeb_war_exploded/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mkmfc
+  Date: 2023/5/2
+  Time: 9:37
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>第一个JavaWeb</title>
+  </head>
+  <body>
+  <h1 style="color: chocolate">Hello,my first JavaWeb</h1>
+  </body>
+</html>
---
+++ src/com/atguigu/java/HelloWorld.java
@@ -0,0 +1,28 @@
+package com.atguigu.java;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Description:
+ *
+ * @author Xie WenJing
+ * @date 2023/4/30 21:51
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        ArrayList myList = new ArrayList();
+        Date date = new Date();
+
+        myList.add(1,1);
+        myList.add(2,date);
+
+    }
+
+
+    public void method(){
+
+    }
+}
